4901df19dae1c3a80d5c680125a5c04c94adc1ae
mihirp-0926 <88792907+mihirp-0926@users.noreply.github.com>
2021-10-30 14:25:51 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,4 +2,17 @@
   $username = $_POST['username'];
   $password = $_POST['password'];
 //Connect to Database
+$conn = new mysqli('localhost' ,'root', '','test');
+if($conn->connect_error){
+    die('connection failed : ' .$conn->connect_error);
+
+}
+else{
+    $stmt = $conn->prepare("insert into registation(username, password) values(?,?)");
+    $stmt->bind_param("ssssssi" ,$username, $password);
+    $stmt->execute();
+    echo "registation sucessful";
+    $stmt->close();
+    $stmt->close();
+}
 ?>
